42749940e53fe571c8f72c72963c83278a1357bd
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2024-10-24 15:18:32 +0100

    restyle menu

--- src/app/components/StyledNavigation.js
+++ src/app/components/StyledNavigation.js
@@ -1,44 +1,50 @@
-'use client';
+"use client";
 
 import {
-    UserGroupIcon,
-    HomeIcon,
-    DocumentDuplicateIcon,
-    CakeIcon,
-  } from "@heroicons/react/24/outline"
-  import Link from 'next/link';
-  import { usePathname } from 'next/navigation';
-  import clsx from 'clsx';
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+  CakeIcon,
+  MapIcon
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
-  export default function NavLinks() {
-    const pathname = usePathname();
-    const links = [
-        { name: "Index", path: "/" , icon:HomeIcon},
-        { name: "Home", path: "/home" , icon:HomeIcon},
-        { name: "About", path: "/about" , icon:CakeIcon},
-        { name: "Contents", path: "/contents" , icon:DocumentDuplicateIcon},
-        { name: "Contact us", path: "/contactus" , icon:UserGroupIcon},
-    ]
+export default function NavLinks() {
+  const pathname = usePathname();
+  const links = [
+    { name: "Index", path: "/", icon: HomeIcon },
+    { name: "Map", path: "/home", icon: MapIcon },
+    { name: "About", path: "/about", icon: CakeIcon },
+    { name: "Contents", path: "/contents", icon: DocumentDuplicateIcon },
+    { name: "Contact us", path: "/contactus", icon: UserGroupIcon },
+  ];
 
-      return (
-        <>
-          {links.map((link) => {
-            const LinkIcon = link.icon;
-            return (
-              <Link
-                key={link.name}
-                href={link.path}
-                className={clsx(
-                    'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-                    {
-                      'bg-sky-100 text-blue-600': pathname === link.href,
-                    },
-                  )}              >
-                <LinkIcon className="w-6" />
-                <p className="hidden md:block">{link.name}</p>
-              </Link>
-            );
-          })}
-        </>
-      );
-    }
+  return (
+    <>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-xl flex flex-wrap mx-auto p-4">
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.path}
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium text-black hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 no-underline",
+              {
+                "bg-sky-100 text-blue-600": pathname === link.href,
+              }
+            )}
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+      </div>
+      </nav>
+    </>
+  );
+}
